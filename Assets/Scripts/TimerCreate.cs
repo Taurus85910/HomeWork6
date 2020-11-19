28a0e2bd0acf761adab028079057541669143cea
Kostya <96919923@mail.ru>
2020-11-19 15:29:30 +1000

    1up

---
+++ Assets/Scripts/TimerCreate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerCreate : MonoBehaviour
+{
+    private CreatingRobber[] _portals;
+
+    private void Start()
+    {
+        _portals = gameObject.GetComponentsInChildren<CreatingRobber>();
+        StartCoroutine(Timer());
+    }
+
+    private IEnumerator Timer()
+    {
+        for (int i = 0; i < _portals.Length; i++)
+        {
+            _portals[i].Spawn();
+            
+
+
+            if (i == _portals.Length - 1)
+                 i = -1;
+            yield return new WaitForSeconds(2);
+        }      
+    }
+
+
+
+}
