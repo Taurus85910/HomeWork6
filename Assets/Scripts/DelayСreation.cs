2e8c3a68a36bf289d729a739a381e2d2106d57b4
Kostya <96919923@mail.ru>
2020-11-29 16:25:36 +1000

    5

---
+++ Assets/Scripts/DelayСreation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayСreation : MonoBehaviour
+{
+    [SerializeField] private float _frequency;
+    private CreatingRobber[] _portals;
+
+    private void Start()
+    {
+        _portals = gameObject.GetComponentsInChildren<CreatingRobber>();
+        StartCoroutine(Delay());
+    }
+
+    private IEnumerator Delay()
+    {
+        while(true)
+        { 
+            foreach (var portal in _portals)
+            {
+                portal.Spawn();
+                yield return new WaitForSeconds(_frequency);
+            }
+        } 
+        
+    }
+}
